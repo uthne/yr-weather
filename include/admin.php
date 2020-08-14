eb771d170aadb11289461dfd17597d2888bdb704
Kjetil Uthne Hansen <uthne@me.com>
2020-08-14 10:01:28 +0200

    Version 1.0 cleanup

--- include/admin.php
+++ include/admin.php
@@ -205,14 +205,13 @@ function wpyr_general_section_callback() {
 function wpyr_gen_place_text_render() {
 	$options = wpyr_get_option();
 	?>
-	<input type='text' name='wpyr_settings[wpyr_gen_place_text]' value='<?php echo $options[' wpyr_gen_place_text ']; ?>' placeholder="Norway/Oslo/Oslo/Oslo"> &nbsp;
+	<input type="text" name="wpyr_settings[wpyr_gen_place_text]" value="<?php echo $options['wpyr_gen_place_text']; ?>" placeholder="Norway/Oslo/Oslo/Oslo"> &nbsp;
 	<?php _e( 'See Help-tab in upper right corner on how to get correct place.','yr-weather' );
 }
 function wpyr_gen_offset_select_render() { 
 	$options = wpyr_get_option();
-
-?>
-	<select name='wpyr_settings[wpyr_gen_offset_select]'>
+	?>
+	<select name="wpyr_settings[wpyr_gen_offset_select]">
 		<option value="0" <?php if($options[ 'wpyr_gen_offset_select']=="0" ) echo( ' selected'); ?>>
 			<?php _e('Now','yr-weather'); ?>
 		</option>
@@ -222,91 +221,74 @@ function wpyr_gen_offset_select_render() {
 		</option>
 		<?php } ?>
 	</select>&nbsp;
-	<?php _e( 'Hours offset to forecast.','yr-weather' ); ?><br/>
-	<div style="float:left;margin: 5px 0 0 0;"><em>
-	<!--?php _e( 'Icon Set can also be selected with shorcode attribute.','yr-weather' ); ?></em-->
-</div>
-<?php
+	<?php _e( 'Hours offset to forecast.','yr-weather' ); 
 }
 function wpyr_gen_icon_select_render() { 
 	$options = wpyr_get_option();
-?>
-<select name='wpyr_settings[wpyr_gen_icon_select]'>
-	<option value="thin"<?php if($options['wpyr_gen_icon_select'] == "thin") echo(' selected'); ?>><?php _e('Outline Thin','yr-weather'); ?></option>
-	<option value="normal"<?php if($options['wpyr_gen_icon_select'] == "normal") echo(' selected'); ?>><?php _e('Outline Normal','yr-weather'); ?></option>
-	<option value="fat"<?php if($options['wpyr_gen_icon_select'] == "fat") echo(' selected'); ?>><?php _e('Outline Fat','yr-weather'); ?></option>
-	<option value="black"<?php if($options['wpyr_gen_icon_select'] == "black") echo(' selected'); ?>><?php _e('Black','yr-weather'); ?></option>
-	<option value="color"<?php if($options['wpyr_gen_icon_select'] == "color") echo(' selected'); ?>><?php _e('Yr Color V.3.0','yr-weather'); ?></option> 
-</select>&nbsp;
-<?php _e( 'Select icon set to use as standard.','yr-weather' ); ?><br />
-<div style="float:left;margin: 5px 0 0 0;"><em>
-	<!--?php _e( 'Icon Set can also be selected with shorcode attribute.','yr-weather' ); ?></em-->
-</div>
-<?php
+	?>
+	<select name="wpyr_settings[wpyr_gen_icon_select]">
+		<option value="thin"<?php if($options['wpyr_gen_icon_select'] == "thin") echo(' selected'); ?>><?php _e('Outline Thin','yr-weather'); ?></option>
+		<option value="normal"<?php if($options['wpyr_gen_icon_select'] == "normal") echo(' selected'); ?>><?php _e('Outline Normal','yr-weather'); ?></option>
+		<option value="fat"<?php if($options['wpyr_gen_icon_select'] == "fat") echo(' selected'); ?>><?php _e('Outline Fat','yr-weather'); ?></option>
+		<option value="black"<?php if($options['wpyr_gen_icon_select'] == "black") echo(' selected'); ?>><?php _e('Black','yr-weather'); ?></option>
+		<option value="color"<?php if($options['wpyr_gen_icon_select'] == "color") echo(' selected'); ?>><?php _e('Yr Color V.3.0','yr-weather'); ?></option> 
+	</select>&nbsp;
+	<?php _e( 'Select icon set to use as standard.','yr-weather' ); 
 }
 function wpyr_gen_iconwidth_text_render() { 
 	$options = wpyr_get_option();
 	?>
-<input type='text' name='wpyr_settings[wpyr_gen_iconwidth_text]' value='<?php echo $options['wpyr_gen_iconwidth_text']; ?>'> &nbsp;&nbsp;
-<?php _e( 'Write css width, i.e 100px, 100%, 10vw etc.','yr-weather' );
+	<input type="text" name="wpyr_settings[wpyr_gen_iconwidth_text]" value="<?php echo $options['wpyr_gen_iconwidth_text']; ?>"> &nbsp;&nbsp;
+	<?php _e( 'Write css width, i.e 100px, 100%, 10vw etc.','yr-weather' );
 }
 function wpyr_gen_iconheight_text_render() { 
 	$options = wpyr_get_option();
 	?>
-<input type='text' name='wpyr_settings[wpyr_gen_iconheight_text]' value='<?php echo $options['wpyr_gen_iconheight_text']; ?>'> &nbsp;
-<?php _e( 'Write css height, i.e 100px, 10vw, 10vw etc.','yr-weather' );
+	<input type="text" name="wpyr_settings[wpyr_gen_iconheight_text]" value="<?php echo $options['wpyr_gen_iconheight_text']; ?>"> &nbsp;
+	<?php _e( 'Write css height, i.e 100px, 10vw, 10vw etc.','yr-weather' );
 }
 function wpyr_gen_smallicon_select_render() { 
 	$options = wpyr_get_option();
-?>
-<select name='wpyr_settings[wpyr_gen_smallicon_select]'>
-	<option value="thin"<?php if($options['wpyr_gen_smallicon_select'] == "thin") echo(' selected'); ?>><?php _e('Outline Thin','yr-weather'); ?></option>
-	<option value="normal"<?php if($options['wpyr_gen_smallicon_select'] == "normal") echo(' selected'); ?>><?php _e('Outline Normal','yr-weather'); ?></option>
-	<option value="fat"<?php if($options['wpyr_gen_smallicon_select'] == "fat") echo(' selected'); ?>><?php _e('Outline Fat','yr-weather'); ?></option>
-	<option value="black"<?php if($options['wpyr_gen_smallicon_select'] == "black") echo(' selected'); ?>><?php _e('Black','yr-weather'); ?></option>
-</select>&nbsp;
-<?php _e( 'Icon set for symbols like temperature, wind, direction, barometer, precipitation etc.','yr-weather' ); ?><br />
-<div style="float:left;margin: 5px 0 0 0;"><em>
-	<!--?php _e( 'Icon Set can also be selected with shorcode attribute.','yr-weather' ); ?></em-->
-</div>
-<?php
+	?>
+	<select name="wpyr_settings[wpyr_gen_smallicon_select]">
+		<option value="thin"<?php if($options['wpyr_gen_smallicon_select'] == "thin") echo(' selected'); ?>><?php _e('Outline Thin','yr-weather'); ?></option>
+		<option value="normal"<?php if($options['wpyr_gen_smallicon_select'] == "normal") echo(' selected'); ?>><?php _e('Outline Normal','yr-weather'); ?></option>
+		<option value="fat"<?php if($options['wpyr_gen_smallicon_select'] == "fat") echo(' selected'); ?>><?php _e('Outline Fat','yr-weather'); ?></option>
+		<option value="black"<?php if($options['wpyr_gen_smallicon_select'] == "black") echo(' selected'); ?>><?php _e('Black','yr-weather'); ?></option>
+	</select>&nbsp;
+	<?php _e( 'Icon set for symbols like temperature, wind, direction, barometer, precipitation etc.','yr-weather' ); 
 }
 function wpyr_gen_smalliconwidth_text_render() { 
 	$options = wpyr_get_option();
 	?>
-<input type='text' name='wpyr_settings[wpyr_gen_smalliconwidth_text]' value='<?php echo $options['wpyr_gen_smalliconwidth_text']; ?>'> &nbsp;&nbsp;
-<?php _e( 'Write css width, i.e 50px, 50%, 5vw etc.','yr-weather' );
+	<input type="text" name='wpyr_settings[wpyr_gen_smalliconwidth_text]' value="<?php echo $options['wpyr_gen_smalliconwidth_text']; ?>"> &nbsp;&nbsp;
+	<?php _e( 'Write css width, i.e 50px, 50%, 5vw etc.','yr-weather' );
 }
-
 function wpyr_gen_smalliconheight_text_render() { 
 	$options = wpyr_get_option();
 	?>
-<input type='text' name='wpyr_settings[wpyr_gen_smalliconheight_text]' value='<?php echo $options['wpyr_gen_smalliconheight_text']; ?>'> &nbsp;
-<?php _e( 'Write css height, i.e 50px, 5em, 5vw etc.','yr-weather' );
+	<input type="text" name='wpyr_settings[wpyr_gen_smalliconheight_text]' value="<?php echo $options['wpyr_gen_smalliconheight_text']; ?>"> &nbsp;
+	<?php _e( 'Write css height, i.e 50px, 5em, 5vw etc.','yr-weather' );
 }
-
 function wpyr_gen_colorfilter_checkbox_render() { 
 	$options = wpyr_get_option();
 	?>
-<input type='checkbox' name='wpyr_settings[wpyr_gen_colorfilter_checkbox]' 
-	<?php if(isset($options['wpyr_gen_colorfilter_checkbox'])) checked( $options['wpyr_gen_colorfilter_checkbox'], 1 ); ?> value='1'>&nbsp;
-	<?php _e( 'Add color to icon.','yr-weather' ); ?><br />
-	<?php
+	<input type="checkbox" name='wpyr_settings[wpyr_gen_colorfilter_checkbox]' 
+	<?php if(isset($options['wpyr_gen_colorfilter_checkbox'])) checked( $options['wpyr_gen_colorfilter_checkbox'], 1 ); ?> value="1">&nbsp;
+	<?php _e( 'Add color to icon.','yr-weather' ); 
 }
 function wpyr_gen_iconcolor_text_render() { 
 	$options = wpyr_get_option();
 	?>
-<input type='text' id="wpyr_gen_iconcolor_text" name='wpyr_settings[wpyr_gen_iconcolor_text]' value='<?php echo $options['wpyr_gen_iconcolor_text']; ?>'> &nbsp;
-<?php _e( 'Defines a color tint to icon mask.','yr-weather' );
+	<input type="text" id="wpyr_gen_iconcolor_text" name='wpyr_settings[wpyr_gen_iconcolor_text]' value="<?php echo $options['wpyr_gen_iconcolor_text']; ?>"> &nbsp;
+	<?php _e( 'Defines a color tint to icon mask.','yr-weather' );
 }
-
 function wpyr_gen_reset_checkbox_render() { 
 	$options = wpyr_get_option();
 	?>
-<input type='checkbox' name='wpyr_settings[wpyr_gen_reset_checkbox]' 
-	<?php if(isset( $options['wpyr_gen_reset_checkbox'] )) checked( $options['wpyr_gen_reset_checkbox'], 1 ); ?> value='1'>&nbsp;
-	<?php _e( 'Check to force default options when plugin is deactivated and reactivated.','yr-weather' ); ?><br />
-	<?php
+	<input type="checkbox" name='wpyr_settings[wpyr_gen_reset_checkbox]' 
+	<?php if(isset( $options['wpyr_gen_reset_checkbox'] )) checked( $options['wpyr_gen_reset_checkbox'], 1 ); ?> value="1">&nbsp;
+	<?php _e( 'Check to force default options when plugin is deactivated and reactivated.','yr-weather' ); 
 }
 
 /* ----- TEMPLATES SETTINGS ----- */
@@ -319,70 +301,71 @@ function wpyr_template_css_area_render() {
 	global $wpyr_default_css;
 	$options = wpyr_get_option();
 	?>
-<div id="css_area_container">
-    <div name="css_area" id="css_area" style="border: 1px solid #DFDFDF; -moz-border-radius: 3px; -webkit-border-radius: 3px; border-radius: 3px; width: 80%; height: 200px; position: relative;"></div>
-</div>
-<textarea style="display:none;" id="wpyr_template_css_area" name="wpyr_template_settings[wpyr_template_css_area]"><?php
-	if(!isset($options['wpyr_template_css_area']) 
-		|| $options['wpyr_template_css_area'] != '' ) echo $options['wpyr_template_css_area']; ?></textarea>
-        <?php
-		echo ('<em>');
-		_e( 'This CSS will be written to the &lt;head&gt; tag, but for optimized performance it is best practice to implement CSS-code in your theme.','yr-weather' );
-		echo ('</em>
-		<br>' ); } function wpyr_template_html_area_render() { global $wpyr_default_css; $options = wpyr_get_option(); ?>
-		<div id="html_area_container">
-			<div name="html_area" id="html_area" style="border: 1px solid #DFDFDF; -moz-border-radius: 3px; -webkit-border-radius: 3px; border-radius: 3px; width: 80%; height: 200px; position: relative;"></div>
-		</div>
-		<textarea style="display:none;" id="wpyr_template_html_area" name='wpyr_template_settings[wpyr_template_html_area]'><?php
-	if(!isset($options['wpyr_template_html_area']) 
-		|| $options['wpyr_template_html_area'] != '' ) echo $options['wpyr_template_html_area']; ?></textarea> <br/>
-		<?php  
-		echo ('<strong>');
-		_e( 'Use these tags to include weather data in your layout:','yr-weather' );
-		echo ('</strong><br>' );
-		echo ('[@symbol] [@symbol-url] [@place] [@date] [@date-time] [@time-from] [@time-to] [@offset] [@offset-lowercase]<br>
-		[@description] [@description-lowercase] [@temperature] [@temperature-f] [@precipitation] [@pressure]<br>
-		[@windSpeed] [@windDirection] [@windDirection-int] [@windText] [@windText-lowercase] [@windDegree]<br>
-		[@symbol-temp] [@symbol-wind] [@symbol-dir] [@symbol-precip] [@symbol-pressure]<br>     
-		[@temp-unit] [@temp-unit-f] [@pressure-scale] [@altitude] [@latitude] [@longitude]<br><br>' );
-		echo ('<strong>');
-		_e( 'Translatable texts (also with first letter capitalized):','yr-weather' );
-		echo ('</strong><br>' );
-		echo ('[time] [from] [to] [and] [in] [at] [hour] [hours] [place] [precipitation] [wind] [windspeed] [wind-direction]<br>
-		[temperature] [degrees] [degree-symbol] [pressure] [air-pressure] [atmospheric-pressure] [barometric-pressure]<br>
-		[weather] [the-weather] [altitude] [MAS] [meters-above-sea] [latitude] [longitude]<br><br>' );
-	
+	<div id="css_area_container">
+		<div name="css_area" id="css_area" style="border: 1px solid #DFDFDF; -moz-border-radius: 3px; -webkit-border-radius: 3px; border-radius: 3px; width: 80%; height: 200px; position: relative;"></div>
+	</div>
+	<textarea style="display:none;" id="wpyr_template_css_area" name="wpyr_template_settings[wpyr_template_css_area]">
+		<?php if(!isset($options['wpyr_template_css_area']) || $options['wpyr_template_css_area'] != '' ) echo $options['wpyr_template_css_area']; ?>
+	</textarea>
+    <em>
+		<?php _e( 'This CSS will be written to the &lt;head&gt; tag, but for optimized performance it is best practice to implement CSS-code in your theme.','yr-weather' ); ?>
+	</em><br> 
+		<?php
+}
+function wpyr_template_html_area_render() { 
+	global $wpyr_default_css; 
+	$options = wpyr_get_option(); 
+	?>
+	<div id="html_area_container">
+		<div name="html_area" id="html_area" style="border: 1px solid #DFDFDF; -moz-border-radius: 3px; -webkit-border-radius: 3px; border-radius: 3px; width: 80%; height: 200px; position: relative;"></div>
+	</div>
+	<textarea style="display:none;" id="wpyr_template_html_area" name='wpyr_template_settings[wpyr_template_html_area]'>
+		<?php if(!isset($options['wpyr_template_html_area']) || $options['wpyr_template_html_area'] != '' ) echo $options['wpyr_template_html_area']; ?>
+	</textarea><br/>
+	<strong>
+		<?php _e( 'Use these tags to include weather data in your layout:','yr-weather' ); ?>
+	</strong><br>
+	[@symbol] [@symbol-url] [@place] [@date] [@date-time] [@time-from] [@time-to] [@offset] [@offset-lowercase]<br>
+	[@description] [@description-lowercase] [@temperature] [@temperature-f] [@precipitation] [@pressure]<br>
+	[@windSpeed] [@windDirection] [@windDirection-int] [@windText] [@windText-lowercase] [@windDegree]<br>
+	[@symbol-temp] [@symbol-wind] [@symbol-dir] [@symbol-precip] [@symbol-pressure]<br>     
+	[@temp-unit] [@temp-unit-f] [@pressure-scale] [@altitude] [@latitude] [@longitude]<br><br>' );
+	<strong>
+		<?php _e( 'Translatable texts (also with first letter capitalized):','yr-weather' ); ?>
+	</strong><br>
+	[time] [from] [to] [and] [in] [at] [hour] [hours] [place] [precipitation] [wind] [windspeed] [wind-direction]<br>
+	[temperature] [degrees] [degree-symbol] [pressure] [air-pressure] [atmospheric-pressure] [barometric-pressure]<br>
+	[weather] [the-weather] [altitude] [MAS] [meters-above-sea] [latitude] [longitude]<br><br>
+		<?php
 }
 function wpyr_template_loop_area_render() { 
 	global $wpyr_default_css;
 	$options = wpyr_get_option();
 	?>
-		<div id="loop_area_container">
-			<div name="loop_area" id="loop_area" style="border: 1px solid #DFDFDF; -moz-border-radius: 3px; -webkit-border-radius: 3px; border-radius: 3px; width: 80%; height: 200px; position: relative;"></div>
-		</div>
-		<textarea style="display:none;" id="wpyr_template_loop_area" name='wpyr_template_settings[wpyr_template_loop_area]'><?php
-	if(!isset($options['wpyr_template_loop_area']) 
-		|| $options['wpyr_template_loop_area'] != '' ) echo $options['wpyr_template_loop_area']; ?></textarea> <br/>
-		<?php  
-		echo ('<strong>');
-		_e( 'Use all tags above, and the following to mark start and end of loop:','yr-weather' );
-		echo ('</strong><br>' );
-		echo ('[@loop-start] [@loop-end] <br><br>' );
-	
+	<div id="loop_area_container">
+		<div name="loop_area" id="loop_area" style="border: 1px solid #DFDFDF; -moz-border-radius: 3px; -webkit-border-radius: 3px; border-radius: 3px; width: 80%; height: 200px; position: relative;"></div>
+	</div>
+	<textarea style="display:none;" id="wpyr_template_loop_area" name='wpyr_template_settings[wpyr_template_loop_area]'>
+		<?php if(!isset($options['wpyr_template_loop_area']) || $options['wpyr_template_loop_area'] != '' ) echo $options['wpyr_template_loop_area']; ?>
+	</textarea><br/>
+	<strong>
+		<?php _e( 'Use all tags above, and the following to mark start and end of loop:','yr-weather' ); ?>
+	</strong><br>
+	[@loop-start] [@loop-end] <br><br>
+		<?php
 }
 function wpyr_template_loop_checkbox_render() { 
 	$options = wpyr_get_option();
 	?>
-		<input type='checkbox' name='wpyr_template_settings[wpyr_template_loop_checkbox]' <?php if(isset( $options[ 'wpyr_template_loop_checkbox'])) checked( $options[ 'wpyr_template_loop_checkbox'], 1 ); ?> value='1'>&nbsp;
-		<?php _e( 'Use single template in loop shortcode.','yr-weather' ); ?><br/>
-		<?php
-		}
-
-		function wpyr_template_loopclass_text_render() {
-			$options = wpyr_get_option();
-			?>
-		<input type='text' id="wpyr_template_loopclass_text" name='wpyr_template_settings[wpyr_template_loopclass_text]' value='<?php echo $options[' wpyr_template_loopclass_text ']; ?>'> &nbsp;
-		<?php _e( 'Class of outer container (if loop template is not defined).','yr-weather' );
+	<input type="checkbox" name='wpyr_template_settings[wpyr_template_loop_checkbox]' 
+		   <?php if(isset( $options[ 'wpyr_template_loop_checkbox'])) checked( $options[ 'wpyr_template_loop_checkbox'], 1 ); ?> value="1">&nbsp;
+	<?php _e( 'Use single template in loop shortcode.','yr-weather' ); 
+}
+function wpyr_template_loopclass_text_render() {
+	$options = wpyr_get_option();
+	?>
+	<input type="text" id="wpyr_template_loopclass_text" name='wpyr_template_settings[wpyr_template_loopclass_text]' value="<?php echo $options[' wpyr_template_loopclass_text ']; ?>"> &nbsp;
+	<?php _e( 'Class of outer container (if loop template is not defined).','yr-weather' );
 }
 
 /* ----- SHORTCODES ----- */
@@ -522,7 +505,7 @@ function wpyr_weather_options_page() {
 	?>
 <div class="wrap">
 	<?php wpyr_options_tabs(); ?>
-	<form id='yr_weather_options' action='options.php' method='post'>
+	<form id="yr_weather_options" action="options.php" method="post">
 
 		<?php 
 	settings_fields( $current_tab );
